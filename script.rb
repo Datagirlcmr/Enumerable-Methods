16dc7f954e78be48d43d0ff50b661218cfbcaab5
Selma Datagirl <datagirlcmr@gmail.com>
2019-11-25 06:32:13 -0600

    added the my_any, my_all, and my_none methods

--- script.rb
+++ script.rb
@@ -4,7 +4,7 @@ module Enumerable
       while counter < self.size
         yield(self[counter])
         counter += 1
-       end
+      end
        self
     end
   
@@ -13,10 +13,103 @@ module Enumerable
       while index < self.size
         yield(index, self[index])
         index += 1
-       end
+      end
        self
     end
-  
+
+    def my_select
+        if block_given?
+          arr = []
+          self.my_each do |x|
+            if yield(x)
+              arr << x
+            end
+          end
+          arr
+        end
+        self
+    end
+
+    def my_all?(pattern = nil)
+        if block_given?
+          self.my_each do |x|
+            return false unless yield(x)
+          end
+        elsif pattern != nil
+          if pattern.is_a?(Class)
+            self.my_each do |x|
+              return false unless x.is_a?(pattern)
+            end
+          elsif pattern.is_a?(Regexp)
+            self.my_each do |x|
+              return false unless pattern.match(x.to_s)
+            end
+          else
+            self.my_each do |x|
+              return false unless x == pattern
+            end
+          end
+        else
+          self.my_each do |x|
+            return false unless x
+          end
+        end
+        true
+    end
+
+    def my_any?(path = nil)
+        if block_given?
+          self.my_each do |x|
+            return true if yield(x)
+          end
+        elsif path != nil
+          if path.is_a?(Class)
+            self.my_each do |x|
+              return true if x.is_a?(path)
+            end
+          elsif path.is_a?(Regexp)
+            self.my_each do |x|
+              return true if path.match(x.to_s)
+            end
+          else
+            self.my_each do |x|
+              return true if x == path
+            end
+          end
+        else
+          self.my_each do |x|
+            return true if x
+          end
+        end
+        false
+      end
+
+      def my_none?(pat = nil)
+        if block_given?
+          self.my_each do |x|
+            return false if yield(x)
+          end
+        elsif pat != nil
+          if pat.is_a?(Class)
+            self.my_each do |x|
+              return false if x.is_a?(pat)
+            end
+          elsif pat.is_a?(Regexp)
+            self.my_each do |x|
+              return false if pat.match(x.to_s)
+            end
+          else
+            self.my_each do |x|
+              return false if x == pat
+            end
+          end
+        else
+          self.my_each do |x|
+            return false if x
+          end
+        end
+        true
+      end
   
   
   end
