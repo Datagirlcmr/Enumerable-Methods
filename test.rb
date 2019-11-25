c9e5a24874d3aa8bbadf1dd67ac93b6a521a0d19
Selma Datagirl <datagirlcmr@gmail.com>
2019-11-25 14:46:30 -0600

    checked the functionality of my methods

--- test.rb
+++ test.rb
@@ -73,43 +73,6 @@ module Enumerable
     end
     total
   end
-
-  def my_map
-    arr = []
-    if proc.nil?
-      my_each { |x| arr << yield(x) }
-    else
-      my_each { |x| arr << proc.call(x) }
-    end
-    arr
-  end
-
-  def my_inject(arg_1 = nil, arg_2 = nil)
-    (inject, sym, array) = get_inject_and_sym(arg_1, arg_2, to_a.dup, block_given?)
-    array.my_each { |i| inject = sym ? inject.send(sym, i) : yield(inject, i) }
-    inject
-  end
-
-  def get_inject_and_sym(arg1, arg2, arr, block)
-    arg1 = arr.shift if arg1.nil? && block
-    return [arg1, nil, arr] if block
-    return [arr.shift, arg1, arr] if arg2.nil?
-
-    [arg1, arg2, arr]
-  end
-
-  def multiply_els(arr)
-    arr.inject(1) { |memo, vals| memo * vals }
-  end
-
-  def check_validity(entry, param)
-    return entry.is_a?(param) if param.is_a?(Class)
-
-    if param.is_a?(Regexp)
-      return false if entry.is_a?(Numeric)
-
-      return param.match(entry)
-    end
-    (entry == param)
-  end
 end
+
+puts [4, 47, 9, 16].my_count
